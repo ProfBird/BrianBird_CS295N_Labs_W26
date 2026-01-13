b0a7bdaacbf0ae6540e4d7985eb7dad84212c73b
Brian Bird <birdb@lanecc.edu>
2026-01-13 14:00:10 -0800

    Added lab 2 demo code

---
+++ CodeReviews/CodeReviews/Controllers/ReviewController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CodeReviews.Controllers
+{
+    public class ReviewController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ReviewRequests()
+        {
+            return View();
+        }
+    }
+}
