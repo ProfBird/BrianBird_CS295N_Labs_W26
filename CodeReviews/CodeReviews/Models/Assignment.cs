38ba50c1834d09597c30fc2bcbe0c4fed4ec27c0
Brian Bird <birdb@lanecc.edu>
2026-02-10 13:10:00 -0800

    Adding Db stuff

---
+++ CodeReviews/CodeReviews/Models/Assignment.cs
@@ -0,0 +1,9 @@
+﻿namespace CodeReviews.Models
+{
+    public class Assignment
+    {
+        public int AssignmentId { get; set; }
+        public string Name { get; set; }
+        public DateOnly DueDate { get; set; }
+    }
+}
