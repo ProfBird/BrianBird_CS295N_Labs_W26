a7d41e403d9464fcccdeb98b4807b954b9018a0a
Brian Bird <ProfBird@users.noreply.github.com>
2026-02-10 14:26:07 -0800

    Added dB code to Program.cs

--- CodeReviews/CodeReviews/Program.cs
+++ CodeReviews/CodeReviews/Program.cs
@@ -1,7 +1,13 @@
+using CodeReviews.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+var connectionString = builder.Configuration.GetConnectionString("MySqlConnection");
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
 var app = builder.Build();
 
