76c61ea0f1b5373995e8da8c183f24a09daed2c2
Brian Bird <ProfBird@users.noreply.github.com>
2026-02-10 16:44:44 -0800

    Added more models

--- CodeReviews/CodeReviews/Models/Assignment.cs
+++ CodeReviews/CodeReviews/Models/Assignment.cs
@@ -3,7 +3,11 @@
     public class Assignment
     {
         public int AssignmentId { get; set; }
-        public string Name { get; set; }
-        public DateOnly DueDate { get; set; }
+        public Section SectionId { get; set; }
+        public string Name { get; set; }  // Ex: Lab01-Variables
+        public DateOnly BetaDueDate { get; set; }
+        public DateOnly ReviewDueDate { get; set; }
+        public DateOnly ProductionDueDate { get; set; }
+        public List<string> Versions { get; set; }  // Ex: A, B, C
     }
 }
---
+++ CodeReviews/CodeReviews/Models/Course.cs
@@ -0,0 +1,8 @@
+namespace CodeReviews.Models;
+
+public class Course
+{
+    public string CoursePrefix { get; set; }  // Ex: CS
+    public string CourseNumber { get; set; }  // Ex: 161
+    public string CourseName { get; set; }   // Ex: Beginning Python
+}
