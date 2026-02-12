d1381c42e16d14e87a0eb2bc7239c573068dda34
Brian Bird <ProfBird@users.noreply.github.com>
2026-02-12 11:56:00 -0800

    Refactored some models

---
+++ CodeReviews/CodeReviews/Migrations/20260210224023_Initial.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CodeReviews.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "AppUsers",
+                columns: table => new
+                {
+                    AppUserId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppUsers", x => x.AppUserId);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Assignments",
+                columns: table => new
+                {
+                    AssignmentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    DueDate = table.Column<DateOnly>(type: "date", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Assignments", x => x.AssignmentId);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AppUsers");
+
+            migrationBuilder.DropTable(
+                name: "Assignments");
+        }
+    }
+}
--- CodeReviews/CodeReviews/Models/Assignment.cs
+++ CodeReviews/CodeReviews/Models/Assignment.cs
@@ -3,11 +3,11 @@
     public class Assignment
     {
         public int AssignmentId { get; set; }
-        public Section SectionId { get; set; }
-        public string Name { get; set; }  // Ex: Lab01-Variables
-        public DateOnly BetaDueDate { get; set; }
+        public required Section ClassSection { get; set; }
+        public required string AssignmentName { get; set; }  // Ex: Lab01-Variables
+        public DateOnly DraftDueDate { get; set; }
         public DateOnly ReviewDueDate { get; set; }
-        public DateOnly ProductionDueDate { get; set; }
-        public List<string> Versions { get; set; }  // Ex: A, B, C
+        public DateOnly FinalDueDate { get; set; }
+        public List<string>? Versions { get; set; }  // Ex: A, B, C
     }
 }
