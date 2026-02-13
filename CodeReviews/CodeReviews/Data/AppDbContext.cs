f95ed68430a2e260ca2ceaccbe3b7778ee729af5
Brian Bird <ProfBird@users.noreply.github.com>
2026-02-12 17:00:36 -0800

    Finished adding review entry page, display page and database stuff.

--- CodeReviews/CodeReviews/Data/AppDbContext.cs
+++ CodeReviews/CodeReviews/Data/AppDbContext.cs
@@ -12,5 +12,6 @@ namespace CodeReviews.Data
         // one DbSet for each domain model class
         public DbSet<AppUser> AppUsers { get; set; }
         public DbSet<Assignment> Assignments { get; set; }
+        public DbSet<Review> Reviews { get; set; }
     }
 }
